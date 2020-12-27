abe7c9b6eb85658a8a055c03d83a81e98f206803
reiven <seventhtrigger0@gmail.com>
2020-12-28 01:24:56 +0900

    [add] git repo installation

---
+++ git_repo_installation.sh
@@ -0,0 +1,20 @@
+rm -rdf ~/dotfiles/terminal-app
+git clone https://github.com/dracula/terminal-app terminal-app
+
+rm -rdf ~/dotfiles/source/enhancd
+git clone https://github.com/b4b4r07/enhancd.git source/enhancd
+
+rm -rdf ~/dotfiles/vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim ~/dotfiles/vim/bundle/Vundle.vim
+
+rm -rdf ~/dotfiles/vim/bundle/vim-airline
+git clone https://github.com/vim-airline/vim-airline.git ~/dotfiles/vim/bundle/vim-airline
+
+rm -rdf ~/dotfiles/vim/bundle/vim-airline-themes/
+git clone https://github.com/vim-airline/vim-airline-themes.git ~/dotfiles/vim/bundle/vim-airline-themes
+
+rm -rdf ~/dotfiles/vim/bundle/nerdtree/
+git clone https://github.com/preservim/nerdtree.git ~/dotfiles/vim/bundle/nerdtree
+
+rm -rdf ~/dotfiles/vim/bundle/vim-polyglot
+git clone https://github.com/sheerun/vim-polyglot.git ~/dotfiles/vim/bundle/vim-polyglot
